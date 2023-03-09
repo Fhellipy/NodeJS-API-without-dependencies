bf3fc552e64cd0277fe37852caaad2817f953e44
Fhellipy Conceição <ffhellipyc.santana@gmail.com>
2023-03-09 15:24:49 -0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
 const http = require("http");
-const users = require("./mocks/users");
+const { URL } = require("url");
+
+const routes = require("./routes");
+const bodyParser = require("./helpers/bodyParser");
 
 const server = http.createServer((request, response) => {
-  if (request.url === "/users" && request.method === "GET") {
-    response.writeHead(200, { "Content-Type": "application/json" });
+  const parsedUrl = new URL(`http://localhost:3000${request.url}`);
 
-    response.end(JSON.stringify(users));
+  let { pathname } = parsedUrl;
+  let id = null;
+
+  const splitEndpoint = pathname.split("/").filter(Boolean);
+
+  if (splitEndpoint.length > 1) {
+    pathname = `/${splitEndpoint[0]}/:id`;
+    id = splitEndpoint[1];
+  }
+
+  const route = routes.find((routeObj) => {
+    return routeObj.endpoint === pathname && routeObj.method === request.method;
+  });
+
+  if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams);
+    request.params = { id };
+
+    response.send = (statusCode, body) => {
+      response.writeHead(statusCode, { "Content-Type": "application/json" });
+      response.end(JSON.stringify(body));
+    };
+
+    if (["POST", "PUT", "PATCH"].includes(request.method)) {
+      bodyParser(request, () => {
+        route.handler(request, response);
+      });
+    } else {
+      route.handler(request, response);
+    }
   } else {
     response.writeHead(404, { "Content-Type": "text/html" });
 
-    response.end(`Cannot ${request.method} ${request.url}`);
+    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
   }
 });
 
---
+++ src/routes.js
@@ -0,0 +1,29 @@
+const UserController = require("./controllers/UserController");
+
+module.exports = [
+  {
+    endpoint: "/users",
+    method: "GET",
+    handler: UserController.listUsers,
+  },
+  {
+    endpoint: "/users/:id",
+    method: "GET",
+    handler: UserController.getUserById,
+  },
+  {
+    endpoint: "/users",
+    method: "POST",
+    handler: UserController.createUser,
+  },
+  {
+    endpoint: "/users/:id",
+    method: "PUT",
+    handler: UserController.updateUser,
+  },
+  {
+    endpoint: "/users/:id",
+    method: "DELETE",
+    handler: UserController.deleteUser,
+  },
+];
